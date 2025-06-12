baff642077db0cc79a23c34e71ee4bbcfeab4bab
alexandrefg11 <victoralexandrefigueiredo6@gmail.com>
2025-06-12 19:20:40 +0000

    Atividade Avaliativa1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { MyGrid } from "./components/MyGrid";
-
+import {Cloudy} from "lucide-react"
 export default function App() {
 
   return (
     //React Fragment
     <>
       <MyGrid />
+    <Cloudy />
     </>
   );
 }
+
--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -3,30 +3,34 @@ import styles from "./MyGrid.module.css";
 export function MyGrid() {
   return (
     <div className={styles.container}>
-      <header className={styles.header1} />
-      <header className={styles.header2} />
-      <aside className={styles.aside} />
+      <header className={styles.header1}/>
+      
       <div className={styles.main}>
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h2>Card 1</h2>
-            <p>This is the first card.</p>
+            <img src="https://picsum.photos/200/300?random=1"></img>
+            <h2>text0</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 2</h2>
-            <p>This is the second card.</p>
+            <img src="https://picsum.photos/200/300?random=2"></img>
+            <h2>text1</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 3</h2>
-            <p>This is the third card.</p>
+            <img src="https://picsum.photos/200/300?random=3"></img>
+            <h2>text2</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 4</h2>
-            <p>This is the fourth card.</p>
+            <img src="https://picsum.photos/200/300?random=4"></img>
+            <h2>text3</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 5</h2>
-            <p>This is the fifth card.</p>
+            <img src="https://picsum.photos/200/300?random=5"></img>
+            <h2>text4</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           </div>
         </div>
       </div>
